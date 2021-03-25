a7456cb5cbd94099700acd167b754ddb7124a36b
Paphatsinee <paphatsinee.r@ku.th>
2021-03-25 17:54:36 +0700

    Update Cube Free Numbers.c

--- Cube Free Numbers.c
+++ Cube Free Numbers.c
@@ -1,28 +1,12 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
     int i,n,k;
     int a[100];
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            for(int b=0;b<=n;b++)
-            {
-                if(j=pow(i,3))
-                {
-                    a[b]=j;
-                }
-            }
-
-        }
-
-    }
     if(1<=n<=1000000)
     {
-        if(n==8||n==16||n==24||n==27||n==32)
+        if(n==8||n==27||n==64||n==125||n==216)
         {
             printf("Not Cube Free");
         }
@@ -31,7 +15,7 @@ int main()
             for(int i=0;i<n;i++)
             {
                 k++;
-                if(i==8||i==16||i==24||i==27||i==32)
+                if(i==8||i==27||i==64||i==125||i==216)
                 {
                     k--;
                 }
