4a9331a93628bcc4d4459d03dd798f6453a6c507
Paphatsinee <paphatsinee.r@ku.th>
2021-03-25 19:50:45 +0700

    Update Silk Texture.c

--- Silk Texture.c
+++ Silk Texture.c
@@ -1,11 +1,56 @@
 #include<stdio.h>
 int main()
 {
-    int M,i,N,P[10],Q[10],R[10];
+    int M,i,j,N,P[10],Q[10],R[10],t,q,r;
     scanf("%d",&M);
     scanf("%d",&N);
-    for(i=0;i<N;i++)
+    char a[M][70];
+    for(i=1;i<=N;i++)
     {
         scanf("%d %d %d",&P[i],&Q[i],&R[i]);
     }
+    for(i=1;i<=N;i++)
+    {
+        P[j]=P[i+1];
+        Q[j]=Q[i+1];
+        R[j]=R[i+1];
+        if(P[i]>P[j])
+        {
+            t=P[i];
+            P[i]=P[j];
+            P[j]=t;
+
+            q=Q[i];
+            Q[i]=Q[j];
+            Q[j]=q;
+
+            r=R[i];
+            R[i]=R[j];
+            R[j]=r;
+        }
+    }
+    for(i=1;i<=M;i++)
+    {
+        for(j=1;j<=70;j++)
+        {
+            if(P[i]==i && Q[j]==j)
+            {
+                for(int z=0;z<R[i];z++)
+                {
+                    printf("x");
+                    j+=R[i];
+                }
+            }
+            else
+            {
+                for(int h=0;h<j;h++)
+                    printf("o");
+            }
+
+        }
+        printf("\n");
+    }
+
+
+return 0;
 }
