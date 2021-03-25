2946f18134cfca74db2be6845ccf57e0d7bfb806
Paphatsinee <paphatsinee.r@ku.th>
2021-03-25 19:07:03 +0700

    Create Silk Texture.c

---
+++ Silk Texture.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int M,i,N,P[10],Q[10],R[10];
+    scanf("%d",&M);
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d %d %d",&P[i],&Q[i],&R[i]);
+    }
+}
